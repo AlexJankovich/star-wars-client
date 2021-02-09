4bf74009d25d5c920ab83b9cc26e94b0cfb0549e
Alexandr <mylo-box-27@yandex.ru>
2021-02-09 12:48:00 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,57 @@
-import React from "react";
+import React, {ChangeEvent, useEffect, useState} from "react";
 import "./App.css";
-import {Container} from "@material-ui/core";
+import {Container, TextField} from "@material-ui/core";
 import {BrowserRouter, Route} from "react-router-dom"
 import {ResourceContainer} from "./components/ResourceContainer";
 import {DetailedInfo} from "./components/DetailedInfo";
 import {routes} from "./api/routes";
 
 function App() {
+  const [isDisabled, setIsDisabled] = useState<boolean>(true)
+  const [inputValue, setInputValue] = useState<string>("")
 
+  const onInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(() => e.target.value)
+  }
+
+  useEffect(() => {
+    if (inputValue.length >= 3) {
+      setIsDisabled(false)
+    } else if (inputValue.length < 3) {
+      setIsDisabled(true)
+    }
+  }, [inputValue])
   return (
     <BrowserRouter>
       <div className="App">
+
+        <TextField
+          label='input value'
+          onChange={onInput}
+          value={inputValue}
+        />
+
         <Container>
+
           <>
-            <Route exact path="/" render={() => <ResourceContainer/>}/>
-            {routes.map(rout => {
-              return <Route
-                key={rout.endPoint}
-                path={`/${rout.endPoint}/:id?`}
-                render={() => <DetailedInfo endPoint={rout.endPoint}/>}/>
-            })}
+            {isDisabled
+              ? null
+              : <>
+                <Route
+                  exact path="/"
+                  render={() => <ResourceContainer searchValue={inputValue}/>}/>
+                {routes.map(rout => {
+                  return <Route
+                    key={rout.endPoint}
+                    path={`/${rout.endPoint}/:id?`}
+                    render={() => <DetailedInfo endPoint={rout.endPoint}/>}/>
+                })}
+              </>
+            }
           </>
+
         </Container>
+
       </div>
     </BrowserRouter>
   );
--- src/components/ResourceContainer.tsx
+++ src/components/ResourceContainer.tsx
@@ -1,39 +1,22 @@
-import {Container, TextField} from "@material-ui/core";
+import {Container} from "@material-ui/core";
 import {ShortInfo} from "./ShortInfo";
-import React, {ChangeEvent, useEffect, useState} from "react";
+import React from "react";
 import {routes} from "../api/routes";
 
-export const ResourceContainer = () => {
-  const [isDisabled, setIsDisabled] = useState<boolean>(true)
-  const [inputValue, setInputValue] = useState<string>("")
+type ResourceContainerType = {
+  searchValue:string
+}
 
-  const onInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputValue(() => e.target.value)
-  }
-
-  useEffect(() => {
-    if (inputValue.length >= 3) {
-      setIsDisabled(false)
-    } else if (inputValue.length < 3) {
-      setIsDisabled(true)
-    }
-  }, [inputValue])
+export const ResourceContainer = ({searchValue}:ResourceContainerType) => {
 
   return (
     <Container>
-      <TextField
-        label='input value'
-        onChange={onInput}
-        value={inputValue}
-      />
-      {isDisabled
-        ? null
-        : routes.map((rout, index) => {
+      {routes.map((rout, index) => {
           return <ShortInfo
             key={rout.endPoint + index}
             endPoint={rout.endPoint}
             value={rout.key}
-            searchValue={inputValue}
+            searchValue={searchValue}
           />
         })
       }
